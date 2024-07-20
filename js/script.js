0b11ef91884d0cccec5141b0861eacef1dc2aa7e
ear-123 <elias.a.rajala@gmail.com>
2024-07-20 15:57:11 +0300

    s

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ const querryJSON = {"query": [    {        "code": "Vuosi",        "selection": 
 }
 
 async function getData() {
-    console.log("sets");
     const url = "https://statfin.stat.fi/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12dy.px"
     const response = await fetch(url, {
         method: "POST",
@@ -19,7 +18,26 @@ async function getData() {
 
     const data = await response.json()
 
-    console.log(data);
+    //console.log(data)
+    //console.log(data.value.toString())
+    const formatedData = {
+        labels: ["2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021"],
+        datasets: [
+            {
+                name: "data", type: "line",
+                values: data.value
+            }
+        ]
+    }
+
+    const chart = new frappe.Chart("#chart", {  // or a DOM element,
+                                                // new Chart() in case of ES6 module with above usage
+        title: "My Awesome Chart",
+        data: formatedData,
+        type: 'line', // or 'bar', 'line', 'scatter', 'pie', 'percentage'
+        height: 450,
+        colors: ['#eb5146']
+    })
 }
 
 getData()
